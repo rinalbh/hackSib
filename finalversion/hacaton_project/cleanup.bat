30876e62b90b5c10f3fe215639937c2a19b53d29
KRIEGER <rinalbh@yandex.ru>
2025-11-30 12:12:15 +0300

    Delete finalversion/hacaton_project directory

--- finalversion/hacaton_project/cleanup.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-echo Очистка временных файлов...
-timeout /t 3 /nobreak >nul
-rmdir /s /q "local_s3_storage" 2>nul
-rmdir /s /q "temp_videos" 2>nul  
-rmdir /s /q "temp" 2>nul
-echo Готово!
-pause
